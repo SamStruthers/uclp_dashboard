8af24d4b825dcec53baf7d7e782f32431f6e41b0
SamStruthers <sam.struthers2000@gmail.com>
2026-06-05 08:36:03 -0600

    adding a backup so that it tries again if it times out

--- R/pull_contrail_api.R
+++ R/pull_contrail_api.R
@@ -94,7 +94,9 @@ pull_contrail_api <- function(start_DT, end_DT = Sys.time(), username, password,
 
   # Get the login page and extract CSRF token
   login_page_req <- request(contrail_login_url) |>
-    req_cookie_preserve(session_file)
+    req_cookie_preserve(session_file)|>
+    req_timeout(20) |>                 # Increase timeout to 15 seconds
+    req_retry(max_tries = 2, backoff = ~ 5) # Allow for a second attempt after waiting ~ 5s
 
   login_page_resp <- req_perform(login_page_req)
 
@@ -114,7 +116,9 @@ pull_contrail_api <- function(start_DT, end_DT = Sys.time(), username, password,
       login = "login",
       continue = continue_key,  # from end of url
       csrf_token = csrf_token
-    )
+    )|>
+    req_timeout(20) |>                 # Increase timeout to 15 seconds
+    req_retry(max_tries = 2, backoff = ~ 5) # Allow for a second attempt after waiting ~ 5s
 
   login_resp <- req_perform(login_req)
 
